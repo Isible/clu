4264e0c950ee8d673e67cb71643b6c74a7e88e63
thepigcat <benpospo@gmail.com>
2023-11-30 21:23:17 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 // Mechanisms for interacting with files
-mod file_handler;
+pub mod file_handler;
 // Trait for getting the literal value of a struct
-mod literal;
+pub mod literal;
 // Mechanisms for throwing and displaying errors
-mod error;
+pub mod error;
 // Utility functions required by
 // different parts of the library
-mod util;
+pub mod util;
 // tests
-mod tests;
+pub mod tests;
